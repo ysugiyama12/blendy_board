9ea22a27944c3a3979cc872d537ab64423c227b5
ysugiyama12 <yuji9511yuji@gmail.com>
2018-11-20 10:30:28 +0900

    alter table

--- cms/models.py
+++ cms/models.py
@@ -4,7 +4,7 @@ class Post(models.Model):
     """投稿"""
     name = models.CharField('タイトル', max_length=255)
     author = models.CharField('投稿者', max_length=255)
-    content = models.CharField('本文', max_length=2000)
+    content = models.TextField('本文', blank=True)
 
     def __str__(self):
         return self.name
